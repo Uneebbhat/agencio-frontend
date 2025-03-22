84cb384a566ced5dd244fc207280dc88ab3305d7
Uneeb Bhatti <uneebbhatti3@gmail.com>
2025-03-22 23:04:58 +0500

    refactor: redirect user from home to dashboard is user is logged in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,19 @@
+"use client";
+
+import useUserStore from "@/store/useUserStore";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+  const router = useRouter();
+  const user = useUserStore((state) => state.getUser());
+
+  useEffect(() => {
+    if (user) {
+      router.replace("/dashboard");
+    }
+  }, [user, router]);
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
