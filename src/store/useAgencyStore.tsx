301263bace3456d7a75b7d06679e1c4c0a86d031
Uneeb Bhatti <uneebbhatti3@gmail.com>
2025-03-30 23:12:31 +0500

    agencyStore removed on logout

--- src/store/useAgencyStore.tsx
+++ src/store/useAgencyStore.tsx
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
-interface Agency {
+export interface Agency {
   userId: string;
   id: string;
   agencyLogo?: File | null;
