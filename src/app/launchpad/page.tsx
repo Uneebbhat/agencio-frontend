5614575e2d47a0c3ee03968d26d40a51ea1e7cc9
Uneeb Bhatti <uneebbhatti3@gmail.com>
2025-09-17 15:24:08 +0500

    new project initiated

--- src/app/launchpad/page.tsx
+++
@@ -1,9 +0,0 @@
-import LaunchpadData from "@/components/Launchpad/LaunchpadData";
-
-export default function LaunchpadPage() {
-  return (
-    <section className="p-6 flex items-center justify-center h-[80dvh]">
-      <LaunchpadData />
-    </section>
-  );
-}
